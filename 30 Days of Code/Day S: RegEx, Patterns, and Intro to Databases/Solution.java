053a0a67a4264bbc5de4b8dacf57f6b23965a41e
Shubham Chaudhary <56770022+skchaudhari903@users.noreply.github.com>
2019-11-18 10:21:07 +0530

    Create Solution.java

---
+++ 30 Days of Code/Day S: RegEx, Patterns, and Intro to Databases/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String emailRegEx = ".+@gmail\\.com$";
+        Pattern pattern = Pattern.compile(emailRegEx);
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int NItr = 0; NItr < N; NItr++) {
+            String[] firstNameEmailID = scanner.nextLine().split(" ");
+
+            String firstName = firstNameEmailID[0];
+
+            String emailID = firstNameEmailID[1];
+
+            Matcher matcher = pattern.matcher(emailID);
+
+            if (matcher.find()) list.add(firstName);
+        }
+        
+        Collections.sort(list);
+
+        list.forEach(System.out::println);
+
+        scanner.close();
+    }
+}
