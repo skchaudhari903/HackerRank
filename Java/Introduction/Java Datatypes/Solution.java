595f250f9276a5cfea4d8a5a67ff1564bce439ce
Shubham Chaudhary <skchaudhari903@gmail.com>
2019-12-22 15:11:57 +0530

    Modified Java-Introduction

--- Java/Introduction/Java Datatypes/Solution.java
+++ Java/Introduction/Java Datatypes/Solution.java
@@ -20,6 +20,7 @@ class Solution{
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
+                //Complete the code
                 if(x>=-32768 && x<=32767)System.out.println("* short");
                 if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
                 System.out.println("* long");
