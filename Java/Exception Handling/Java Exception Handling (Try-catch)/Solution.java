63b02ef81b86e47ac1e8f4a440df62467973e641
Shubham Chaudhary <skchaudhari903@gmail.com>
2019-12-26 10:32:40 +0530

    Push HackerRank-JavaExceptionHandling

---
+++ Java/Exception Handling/Java Exception Handling (Try-catch)/Solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+    }
+}
