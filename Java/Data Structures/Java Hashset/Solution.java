ed3de667d8488bf05019f77fbbf93d31155991dc
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-11-16 17:06:46 +0530

    Push Java Hashset sol

---
+++ Java/Data Structures/Java Hashset/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+        HashSet<String> h = new HashSet<String>();
+        for (int i = 0; i < t; i++) {
+            h.add(pair_left[i] + " " + pair_right[i]);
+            System.out.println(h.size());
+        }
+   }
+}
