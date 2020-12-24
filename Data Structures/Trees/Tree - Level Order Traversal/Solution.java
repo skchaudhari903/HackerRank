76a8840d696c9bd9e56b3a309a5f0fc729e09c32
Shubham Chaudhary <skchaudhari903@gmail.com>
2020-12-24 19:50:56 +0530

    Push Level Order Traversal solution

---
+++ Data Structures/Trees/Tree - Level Order Traversal/Solution.java
@@ -0,0 +1,69 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+	/* 
+    
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static void levelOrder(Node root) {
+        ArrayDeque<Node> deque = new ArrayDeque(); // use deque as a queue
+        if (root != null) {
+            deque.add(root);
+        }
+        while (!deque.isEmpty()) {
+            Node n = deque.remove();
+            System.out.print(n.data + " ");
+            if (n.left != null) {
+                deque.add(n.left);
+            }
+            if (n.right != null) {
+                deque.add(n.right);
+            }
+        }
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        levelOrder(root);
+    }	
+}
